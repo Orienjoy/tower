72aa2fcf4de1f5e63611131cb49ca5342d530275
Orienjoy <orienjoy@163.com>
2017-04-16 22:11:24 +0800

    2017/04/16 backup

---
+++ src/main/java/com/orienjoy/tower/controller/RequestHostController.java
@@ -0,0 +1,34 @@
+package com.orienjoy.tower.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.orienjoy.tower.service.IRequestHostService;
+import com.orienjoy.tower.service.impl.RequestHostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Orienjoy on 2017/4/15.
+ */
+@RestController
+public class RequestHostController {
+
+    @Resource
+    private IRequestHostService requestHostService;
+
+    private static Logger logger = LoggerFactory.getLogger(RequestHostService.class);
+
+    // 拼接 env-host/api
+    @RequestMapping(value = "/public/url/{requestId}", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public JSONObject getRequestHostById(HttpServletResponse response, @PathVariable("requestHostId") Long requestHostId){
+        logger.info("Request URL = {} ", requestHostId);
+        JSONObject json = new JSONObject();
+
+        return json;
+    }
+}
