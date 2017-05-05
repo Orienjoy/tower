d12ec1ab19bf716c879f0470f891c4405ab38c52
Orienjoy <orienjoy@163.com>
2017-05-05 12:38:13 +0800

    a little code

--- src/main/java/com/orienjoy/tower/service/IEnvService.java
+++ src/main/java/com/orienjoy/tower/service/IEnvService.java
@@ -1,15 +1,12 @@
 package com.orienjoy.tower.service;
 
 import com.alibaba.fastjson.JSONObject;
-import com.orienjoy.tower.domain.Env;
 
 /**
  * Created by FEI on 2017/5/5.
  */
 public interface IEnvService {
 
-    JSONObject getEnvById(Long id);
-
-    Env getEnvById(Long id);
+    JSONObject getEnvById(Long envId);
 
 }
