0b0041a6e073252d38cdcfd936a35e2c396a8e8c
Orienjoy <orienjoy@163.com>
2017-05-11 09:26:49 +0800

    a little change

---
+++ study/main/java/com/orienjoy/algorithm/Recurse.java
@@ -0,0 +1,24 @@
+package com.orienjoy.algorithm;
+
+/**
+ * Created by orienjoy on 2017/5/11.
+ */
+public class Recurse {
+    int recurse_sum(int m){
+        if(m == 0)
+            return 0;
+        else
+            return recurse_sum(m-1) + m;
+    }
+
+    // 与 循环 比较
+    int circulate_sum(int m){
+        int count = 0;
+        if(m <0)
+            return -1;
+        for(int index = 0; index <= m; index++)
+            count += index;
+        return count;
+    }
+}
+
